3e57917ab9e94e58452a06aecef1fe525224eaaa
Ian Carroll <icarroll@sesync.org>
2018-08-14 20:25:27 -0400

    re-write of lesson-worksheet relationship

--- worksheet.R
+++ worksheet.R
@@ -4,7 +4,7 @@ library(dplyr)
 animals <- read.csv('data/animals.csv', stringsAsFactors = FALSE)
 species <- read.csv('data/species.csv', stringsAsFactors = FALSE)
 
-# group animals captured in 1990 by species_id and summarize with total
+# group animals captured in one year by species_id and summarize with total
 # abundance and average weight ignoring non-rodents
 rodents <- animals %>%
   filter(year == 1990) %>%
@@ -12,3 +12,6 @@ rodents <- animals %>%
   filter(taxa == 'Rodent') %>%
   group_by(species_id) %>%
   summarize(count = n(), weight = mean(weight))
+
+# pretend this takes a really long time!
+Sys.sleep(10)
